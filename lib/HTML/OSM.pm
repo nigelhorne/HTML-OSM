4ea7e42a1d74ef6b5971d82e1034313578454739
Nigel Horne <njh@bandsman.co.uk>
2025-02-16 09:19:59 -0500

    Added the zoom method

--- lib/HTML/OSM.pm
+++ lib/HTML/OSM.pm
@@ -31,7 +31,7 @@ The generated map allows users to view marked locations, zoom, and search for lo
     my $map = HTML::OSM->new();
     # ...
 
-    my $osm = HTML::OSM->new(
+    my $map = HTML::OSM->new(
 	  coordinates => [
 		  [34.0522, -118.2437, 'Los Angeles'],
 		  [undef, undef, 'Paris'],
@@ -44,7 +44,7 @@ The generated map allows users to view marked locations, zoom, and search for lo
 
 =head2 new
 
-    $osm = HTML::OSM->new(
+    $map = HTML::OSM->new(
 	  coordinates => [
 		  [37.7749, -122.4194, 'San Francisco'],
 		  [40.7128, -74.0060, 'New York'],
@@ -53,7 +53,7 @@ The generated map allows users to view marked locations, zoom, and search for lo
 	  zoom => 10,
     );
 
-    $osm->generate_map();
+    $map->generate_map();
 
 Creates a new C<HTML::OSM> object with the provided coordinates and optional zoom level.
 
@@ -131,12 +131,33 @@ sub new
 	}, $class;
 }
 
+=head2 zoom
+
+Get/set the new zoom level (0 is corsest)
+
+    $map->zoom(10);
+
+=cut
+
+sub zoom
+{
+	my $self = shift;
+
+	if(scalar(@_)) {
+		my $params = $self->_get_params('zoom', @_);
+
+		$self->{'zoom'} = $params->{'zoom'};
+	}
+
+	return $self->{'zoom'};
+}
+
 =head2 generate_map
 
 Generates an HTML file (C<map.html>) containing the interactive map with the specified coordinates.
 The file includes basic functionality such as zooming, resetting the map view, and searching locations.
 
-  $osm->generate_map();
+  $map->generate_map();
 
 =cut
 
@@ -469,6 +490,41 @@ sub onload_render
 	};
 
 	return ($head, $body);
+}
+
+# Helper routine to parse the arguments given to a function.
+# Processes arguments passed to methods and ensures they are in a usable format,
+#	allowing the caller to call the function in anyway that they want
+#	e.g. foo('bar'), foo(arg => 'bar'), foo({ arg => 'bar' }) all mean the same
+#	when called _get_params('arg', @_);
+sub _get_params
+{
+	shift;  # Discard the first argument (typically $self)
+	my $default = shift;
+
+	# Directly return hash reference if the first parameter is a hash reference
+	return $_[0] if(ref($_[0]) eq 'HASH');
+
+	my %rc;
+	my $num_args = scalar @_;
+
+	# Populate %rc based on the number and type of arguments
+	if(($num_args == 1) && (defined $default)) {
+		# %rc = ($default => shift);
+		return { $default => shift };
+	} elsif($num_args == 1) {
+		Carp::croak('Usage: ', __PACKAGE__, '->', (caller(1))[3], '()');
+	} elsif(($num_args == 0) && (defined($default))) {
+		Carp::croak('Usage: ', __PACKAGE__, '->', (caller(1))[3], "($default => \$val)");
+	} elsif(($num_args % 2) == 0) {
+		%rc = @_;
+	} elsif($num_args == 0) {
+		return;
+	} else {
+		Carp::croak('Usage: ', __PACKAGE__, '->', (caller(1))[3], '()');
+	}
+
+	return \%rc;
 }
 
 =head1 AUTHOR
